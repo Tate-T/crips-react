a97735f9b274e00f7d884c079f0aa75338efc8e9
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2025-07-19 15:47:58 +0300

    html-product-page

---
+++ src/pages/ProductPage/FaqsList.jsx
@@ -0,0 +1,56 @@
+export const FaqsList = ()=>{
+    return(
+        <>
+        <select name="Details">
+            <option value="apple">
+                <div>
+                    <h3>ABOUT PRODUCT</h3>
+                    <p>
+                        Cool off this summer in the Mini Ruffle Smocked Tank Top from our very own LA Hearts. This tank features a smocked body, adjustable straps, scoop neckline, ruffled hems, and a cropped fit.
+                    </p>
+                </div>
+            </option>
+            <option value="banana"><div>
+                    <h3>ADVANTAGES</h3>
+                    <p>
+                        Smocked body
+                        Adjustable straps
+                        Scoop neckline
+                        Ruffled hems
+                        Cropped length
+                        Model is wearing a small
+                        100% rayon
+                        Machine washable
+                    </p>
+                </div>
+            </option>
+            <option value="orange">
+                <div>
+                    <h3>ADVANTAGES</h3>
+                    <p>
+                        Smocked body
+                        Adjustable straps
+                        Scoop neckline
+                    </p>
+                </div>
+            </option>
+            <option value="orange">
+                <div>
+                    <h3>SHIPPING</h3>
+                    <p>
+                        We offer Free Standard Shipping for all orders over $75 to the 50 states and the District of Columbia. The minimum order value must be $75 before taxes, shipping and handling. Shipping fees are non-refundable.
+                        Please allow up to 2 business days (excluding weekends, holidays, and sale days) to process your order.
+                        Processing Time + Shipping Time = Delivery Time
+                    </p>
+                </div>
+            </option>
+        </select>
+        <select name="Other information">
+            
+        </select>
+        <select name="Another tab">
+            
+        </select>
+        </>
+    )
+}
