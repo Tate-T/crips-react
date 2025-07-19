798c92944f4eb2868dcba9f65afaaa5accfa972b
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2025-07-19 15:30:08 +0300

    rename

--- src/pages/CabinetPage/CreateOrder/ShippingDetails/ShippingDetails.jsx
+++ src/pages/CabinetPage/CreateOrder/ShippingDetails/ShippingDetails.jsx
@@ -1,78 +1,78 @@
-export const ShoppingDetails = () => {
+export const ShippingDetails = () => {
   return (
     <>
       <div>
-        <div className="shopping__wrap">
-          <form className="shop-form">
-            <h2 className="shop-form__title">Shipping Address</h2>
-            <div className="shop-form__row">
+        <div className="shipping__wrap">
+          <form className="ship-form">
+            <h2 className="ship-form__title">Shipping Address</h2>
+            <div className="ship-form__row">
               <label htmlFor="#">
-                Email Address <span className="shop-form__reqired">*</span>
+                Email Address <span className="ship-form__reqired">*</span>
               </label>
               <input
-                className="shop-form__input"
+                className="ship-form__input"
                 type="email"
                 placeholder="roni_cost@example.com"
                 required
               />
             </div>
-            <div className="shop-form__row">
+            <div className="ship-form__row">
               <label htmlFor="#">Password</label>
               <input
-                className="shop-form__input"
+                className="ship-form__input"
                 type="password"
                 placeholder="******************"
               />
             </div>
-            <div className="shop-form__row">
+            <div className="ship-form__row">
               <label htmlFor="#">Zip/Postal Code</label>
-              <input className="shop-form__input" type="number" />
+              <input className="ship-form__input" type="number" />
             </div>
-            <p className="shop-form__text">
+            <p className="ship-form__text">
               You already have an account with us. Sign in or continue as guest.
             </p>
-            <div className="shop-form__btns">
-              <button className="shop-form__btn--login">Login</button>
-              <button className="shop-form__btn--restore">
+            <div className="ship-form__btns">
+              <button className="ship-form__btn--login">Login</button>
+              <button className="ship-form__btn--restore">
                 Forgot Your Password?
               </button>
             </div>
           </form>
-          <div className="shopping__line"></div>
-          <form className="shop-form">
-            <div className="shop-form__row">
+          <div className="shipping__line"></div>
+          <form className="ship-form">
+            <div className="ship-form__row">
               <label htmlFor="#">
-                First Name <span className="shop-form__reqired">*</span>
+                First Name <span className="ship-form__reqired">*</span>
               </label>
-              <input className="shop-form__input" type="text" required />
+              <input className="ship-form__input" type="text" required />
             </div>
-            <div className="shop-form__row">
+            <div className="ship-form__row">
               <label htmlFor="#">
-                Last Name <span className="shop-form__reqired">*</span>
+                Last Name <span className="ship-form__reqired">*</span>
               </label>
-              <input className="shop-form__input" type="text" required />
+              <input className="ship-form__input" type="text" required />
             </div>
-            <div className="shop-form__row">
+            <div className="ship-form__row">
               <label htmlFor="#">Company</label>
-              <input className="shop-form__input" type="text" />
+              <input className="ship-form__input" type="text" />
             </div>
-            <div className="shop-form__row">
+            <div className="ship-form__row">
               <label htmlFor="#">
-                Street Address <span className="shop-form__reqired">*</span>
+                Street Address <span className="ship-form__reqired">*</span>
               </label>
-              <input className="shop-form__input" type="text" required />
-              <input className="shop-form__input" type="text" required />
-              <input className="shop-form__input" type="text" required />
+              <input className="ship-form__input" type="text" required />
+              <input className="ship-form__input" type="text" required />
+              <input className="ship-form__input" type="text" required />
             </div>
-            <div className="shop-form__row">
+            <div className="ship-form__row">
               <label htmlFor="#">
-                Country <span className="shop-form__reqired">*</span>
+                Country <span className="ship-form__reqired">*</span>
               </label>
-              <input className="shop-form__input" type="text" required />
+              <input className="ship-form__input" type="text" required />
             </div>
-            <div className="shop-form__row">
+            <div className="ship-form__row">
               <label htmlFor="#">
-                State/Province <span className="shop-form__reqired">*</span>
+                State/Province <span className="ship-form__reqired">*</span>
               </label>
               <select name="regiond" id="#">
                 <option value="base" disabled selected hidden>
@@ -81,40 +81,40 @@ export const ShoppingDetails = () => {
               </select>
             </div>
           </form>
-          <form className="shop-form">
-            <h2 className="shop-form__title">Shipping Methods</h2>
-            <div className="shop-form__list">
-              <label className="shop-form__item">
+          <form className="ship-form">
+            <h2 className="ship-form__title">Shipping Methods</h2>
+            <div className="ship-form__list">
+              <label className="ship-form__item">
                 <input
-                  className="shop-form__radio"
+                  className="ship-form__radio"
                   type="radio"
                   name="shipping"
                   value="fixed"
                   defaultChecked
                 />
-                <div className="shop-form__methods">
-                  <p className="shop-form__rate">$5.00</p>
-                  <p className="shop-form__type">Fixed</p>
-                  <p className="shop-form__type">Flat Rate</p>
+                <div className="ship-form__methods">
+                  <p className="ship-form__rate">$5.00</p>
+                  <p className="ship-form__type">Fixed</p>
+                  <p className="ship-form__type">Flat Rate</p>
                 </div>
               </label>
-              <label className="shop-form__item shop-form__item--disabled">
+              <label className="ship-form__item ship-form__item--disabled">
                 <input
-                  className="shop-form__radio"
+                  className="ship-form__radio"
                   type="radio"
                   name="shipping"
                   value="table"
                 />
-                <div className="shop-form__methods">
-                  <p className="shop-form__rate">$10.00</p>
-                  <p className="shop-form__type">Table Rate</p>
-                  <p className="shop-form__type">Best Way</p>
+                <div className="ship-form__methods">
+                  <p className="ship-form__rate">$10.00</p>
+                  <p className="ship-form__type">Table Rate</p>
+                  <p className="ship-form__type">Best Way</p>
                 </div>
               </label>
             </div>
-            <div className="shop-form__navigation">
-              <button className="shop-form__btn--next">Next</button>
-              <button className="shop-form__btn--back">Back</button>
+            <div className="ship-form__navigation">
+              <button className="ship-form__btn--next">Next</button>
+              <button className="ship-form__btn--back">Back</button>
             </div>
           </form>
         </div>
