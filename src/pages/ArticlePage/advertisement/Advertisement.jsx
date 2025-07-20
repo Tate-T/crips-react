aa22629a4f941e57ed11d405f17b2d2e6bb0828a
NecoKab2011 <142300074+NecoKab2011@users.noreply.github.com>
2025-07-20 11:08:06 +0200

    add-articles-page

---
+++ src/pages/ArticlePage/advertisement/Advertisement.jsx
@@ -0,0 +1,10 @@
+export const Advertisement = () => {
+    return (
+        <>
+        <div className="advertisement-box">
+            <img src={item.img} alt="" />
+            <img src={item.img} alt="" />
+        </div>
+        </>
+    )
+}
