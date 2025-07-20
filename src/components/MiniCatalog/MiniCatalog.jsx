7f0195c21d818c445fe9030a62093d53216051c4
rocketmike12 <mg.korop@gmail.com>
2025-07-20 14:36:05 +0300

    started doing styles for MiniCatalog

--- src/components/MiniCatalog/MiniCatalog.jsx
+++ src/components/MiniCatalog/MiniCatalog.jsx
@@ -1,21 +1,58 @@
 import { MiniCatalogItem } from "./MiniCatalogItem.jsx";
 import { featuredItemsData, mostPopularData } from "../../data/mini-catalog-data.js";
+import styles from "./MiniCatalog.module.scss";
 
 export const MiniCatalog = () => {
 	return (
 		<>
-			<h2 className="featured-items-title">Featured Items</h2>
-			<ul className="featured-items-list">
-				{featuredItemsData.map((item) => (
-					<MiniCatalogItem img={item.img} category={item.category} title={item.title} price={item.price} />
-				))}
-			</ul>
-			<h2 className="most-popular-title">Most Popular</h2>
-			<ul className="most-popular-list">
-				{mostPopularData.map((item) => (
-					<MiniCatalogItem img={item.img} category={item.category} title={item.title} price={item.price} />
-				))}
-			</ul>
+			<section className={styles["mini-catalog"]}>
+				<div className="container">
+					<div className={`${styles["mini-catalog__list-wrap"]} ${styles["mini-catalog__list-wrap--featured"]}`}>
+						<div className={`${styles["mini-catalog__top-wrap"]} ${styles["mini-catalog__top-wrap--featured"]}`}>
+							<h2 className={`${styles["mini-catalog__title"]} ${styles["mini-catalog__title--featured"]}`}>Featured Items</h2>
+							<div className="mini-catalog__btn-wrap">
+								<button className="mini-catalog__btn">
+									<svg>
+										<use></use>
+									</svg>
+								</button>
+								<button className="mini-catalog__btn">
+									<svg>
+										<use></use>
+									</svg>
+								</button>
+							</div>
+						</div>
+						<ul className={`${styles["mini-catalog__list"]} ${styles["mini-catalog__list--featured"]}`}>
+							{featuredItemsData.map((item) => (
+								<MiniCatalogItem img={item.img} category={item.category} title={item.title} price={item.price} />
+							))}
+						</ul>
+					</div>
+					<div className={`${styles["mini-catalog__list-wrap"]} ${styles["mini-catalog__list-wrap--popular"]}`}>
+						<div className={`${styles["mini-catalog__top-wrap"]} ${styles["mini-catalog__top-wrap--popular"]}`}>
+							<h2 className={`${styles["mini-catalog__title"]} ${styles["mini-catalog__title--popular"]}`}>Most Popular</h2>
+							<div className="mini-catalog__btn-wrap">
+								<button className="mini-catalog__btn">
+									<svg>
+										<use></use>
+									</svg>
+								</button>
+								<button className="mini-catalog__btn">
+									<svg>
+										<use></use>
+									</svg>
+								</button>
+							</div>
+						</div>
+						<ul className={`${styles["mini-catalog__list"]} ${styles["mini-catalog__list--popular"]}`}>
+							{mostPopularData.map((item) => (
+								<MiniCatalogItem img={item.img} category={item.category} title={item.title} price={item.price} />
+							))}
+						</ul>
+					</div>
+				</div>
+			</section>
 		</>
 	);
 };
--- src/components/MiniCatalog/MiniCatalogItem.jsx
+++ src/components/MiniCatalog/MiniCatalogItem.jsx
@@ -1,10 +1,12 @@
+import styles from "./MiniCatalog.module.scss";
+
 export const MiniCatalogItem = ({ img, category, title, price }) => {
 	return (
-		<li>
-			<img className="mini-catalog-list-img" src={img} alt="" />
-			<p className="mini-catalog-list-category">{category}</p>
-			<p className="mini-catalog-list-title">{title}</p>
-			<p className="mini-catalog-list-price">{price + " EUR"}</p>
+		<li className={styles["mini-catalog__item"]}>
+			<img className={styles["mini-catalog__item-image"]} src={img} alt="" />
+			<p className={styles["mini-catalog__item-category"]}>{category}</p>
+			<p className={styles["mini-catalog__item-title"]}>{title}</p>
+			<p className={styles["mini-catalog__item-price"]}>{price + " EUR"}</p>
 		</li>
 	);
 };
