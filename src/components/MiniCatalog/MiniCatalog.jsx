15fe674bf3f80c06e0e7a050680898119899722f
rocketmike12 <mg.korop@gmail.com>
2025-07-20 15:04:04 +0300

    fixed unneeded changes

--- src/components/MiniCatalog/MiniCatalog.jsx
+++ src/components/MiniCatalog/MiniCatalog.jsx
@@ -1,58 +1,30 @@
-import { MiniCatalogItem } from "./MiniCatalogItem.jsx";
 import { featuredItemsData, mostPopularData } from "../../data/mini-catalog-data.js";
-import styles from "./MiniCatalog.module.scss";
 
 export const MiniCatalog = () => {
-	return (
-		<>
-			<section className={styles["mini-catalog"]}>
-				<div className="container">
-					<div className={`${styles["mini-catalog__list-wrap"]} ${styles["mini-catalog__list-wrap--featured"]}`}>
-						<div className={`${styles["mini-catalog__top-wrap"]} ${styles["mini-catalog__top-wrap--featured"]}`}>
-							<h2 className={`${styles["mini-catalog__title"]} ${styles["mini-catalog__title--featured"]}`}>Featured Items</h2>
-							<div className="mini-catalog__btn-wrap">
-								<button className="mini-catalog__btn">
-									<svg>
-										<use></use>
-									</svg>
-								</button>
-								<button className="mini-catalog__btn">
-									<svg>
-										<use></use>
-									</svg>
-								</button>
-							</div>
-						</div>
-						<ul className={`${styles["mini-catalog__list"]} ${styles["mini-catalog__list--featured"]}`}>
-							{featuredItemsData.map((item) => (
-								<MiniCatalogItem img={item.img} category={item.category} title={item.title} price={item.price} />
-							))}
-						</ul>
-					</div>
-					<div className={`${styles["mini-catalog__list-wrap"]} ${styles["mini-catalog__list-wrap--popular"]}`}>
-						<div className={`${styles["mini-catalog__top-wrap"]} ${styles["mini-catalog__top-wrap--popular"]}`}>
-							<h2 className={`${styles["mini-catalog__title"]} ${styles["mini-catalog__title--popular"]}`}>Most Popular</h2>
-							<div className="mini-catalog__btn-wrap">
-								<button className="mini-catalog__btn">
-									<svg>
-										<use></use>
-									</svg>
-								</button>
-								<button className="mini-catalog__btn">
-									<svg>
-										<use></use>
-									</svg>
-								</button>
-							</div>
-						</div>
-						<ul className={`${styles["mini-catalog__list"]} ${styles["mini-catalog__list--popular"]}`}>
-							{mostPopularData.map((item) => (
-								<MiniCatalogItem img={item.img} category={item.category} title={item.title} price={item.price} />
-							))}
-						</ul>
-					</div>
-				</div>
-			</section>
-		</>
-	);
-};
+  return (
+    <>
+      <h2 className="featured-items-title">Featured Items</h2>
+      <ul className="featured-items-list">
+        {featuredItemsData.map((item) => (
+          <li>
+            <img src={item.img} alt="" />
+            <p className="featured-items-list-category">{item.category}</p>
+            <p className="featured-items-list-title">{item.title}</p>
+            <p className="featured-items-list-price">{item.price} EUR</p>
+          </li>
+        ))}
+      </ul>
+      <h2 className="most-popular-title">Most Popular</h2>
+      <ul className="most-popular-list">
+        {mostPopularData.map((item) => (
+          <li>
+            <img src={item.img} alt="" />
+            <p className="most-popular-list-category">{item.category}</p>
+            <p className="most-popular-list-title">{item.title}</p>
+            <p className="most-popular-list-price">{item.price} EUR</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
--- src/components/MiniCatalog/MiniCatalogItem.jsx
+++ src/components/MiniCatalog/MiniCatalogItem.jsx
@@ -1,12 +1,7 @@
-import styles from "./MiniCatalog.module.scss";
-
-export const MiniCatalogItem = ({ img, category, title, price }) => {
+export default MiniCatalogItem = ({ img }) => {
 	return (
-		<li className={styles["mini-catalog__item"]}>
-			<img className={styles["mini-catalog__item-image"]} src={img} alt="" />
-			<p className={styles["mini-catalog__item-category"]}>{category}</p>
-			<p className={styles["mini-catalog__item-title"]}>{title}</p>
-			<p className={styles["mini-catalog__item-price"]}>{price + " EUR"}</p>
+		<li>
+			
 		</li>
 	);
-};
+}
