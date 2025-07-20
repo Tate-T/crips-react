bd2b8f79ae09ab10f591c6edb349abe0d8f770b5
rocketmike12 <mg.korop@gmail.com>
2025-07-20 13:31:38 +0300

    changed mini-catalog structure

--- src/components/MiniCatalog/MiniCatalog.jsx
+++ src/components/MiniCatalog/MiniCatalog.jsx
@@ -1,30 +1,21 @@
+import { MiniCatalogItem } from "./MiniCatalogItem.jsx";
 import { featuredItemsData, mostPopularData } from "../../data/mini-catalog-data.js";
 
 export const MiniCatalog = () => {
-  return (
-    <>
-      <h2 className="featured-items-title">Featured Items</h2>
-      <ul className="featured-items-list">
-        {featuredItemsData.map((item) => (
-          <li>
-            <img src={item.img} alt="" />
-            <p className="featured-items-list-category">{item.category}</p>
-            <p className="featured-items-list-title">{item.title}</p>
-            <p className="featured-items-list-price">{item.price} EUR</p>
-          </li>
-        ))}
-      </ul>
-      <h2 className="most-popular-title">Most Popular</h2>
-      <ul className="most-popular-list">
-        {mostPopularData.map((item) => (
-          <li>
-            <img src={item.img} alt="" />
-            <p className="most-popular-list-category">{item.category}</p>
-            <p className="most-popular-list-title">{item.title}</p>
-            <p className="most-popular-list-price">{item.price} EUR</p>
-          </li>
-        ))}
-      </ul>
-    </>
-  )
-}
+	return (
+		<>
+			<h2 className="featured-items-title">Featured Items</h2>
+			<ul className="featured-items-list">
+				{featuredItemsData.map((item) => (
+					<MiniCatalogItem img={item.img} category={item.category} title={item.title} price={item.price} />
+				))}
+			</ul>
+			<h2 className="most-popular-title">Most Popular</h2>
+			<ul className="most-popular-list">
+				{mostPopularData.map((item) => (
+					<MiniCatalogItem img={item.img} category={item.category} title={item.title} price={item.price} />
+				))}
+			</ul>
+		</>
+	);
+};
--- src/components/MiniCatalog/MiniCatalogItem.jsx
+++ src/components/MiniCatalog/MiniCatalogItem.jsx
@@ -1,7 +1,10 @@
-export default MiniCatalogItem = ({ img }) => {
+export const MiniCatalogItem = ({ img, category, title, price }) => {
 	return (
 		<li>
-			
+			<img className="mini-catalog-list-img" src={img} alt="" />
+			<p className="mini-catalog-list-category">{category}</p>
+			<p className="mini-catalog-list-title">{title}</p>
+			<p className="mini-catalog-list-price">{price + " EUR"}</p>
 		</li>
 	);
-}
+};
