52b7d872ad3efd638b7e5aaa34afeb230d555823
rocketmike12 <mg.korop@gmail.com>
2025-07-26 14:52:15 +0300

    MiniCatalog styles done

--- src/components/MiniCatalog/MiniCatalog.jsx
+++ src/components/MiniCatalog/MiniCatalog.jsx
@@ -1,24 +1,27 @@
+import { Container } from "../Container/Container.jsx";
 import { MiniCatalogItem } from "./MiniCatalogItem.jsx";
 import { featuredItemsData, mostPopularData } from "../../data/mini-catalog-data.js";
+import arrowLeft from "../../images/arrow-left.svg";
+import arrowRight from "../../images/arrow-right.svg";
 import styles from "./MiniCatalog.module.scss";
 
 export const MiniCatalog = () => {
 	return (
 		<>
 			<section className={styles["mini-catalog"]}>
-				<div className="container">
+				<Container>
 					<div className={`${styles["mini-catalog__list-wrap"]} ${styles["mini-catalog__list-wrap--featured"]}`}>
 						<div className={`${styles["mini-catalog__top-wrap"]} ${styles["mini-catalog__top-wrap--featured"]}`}>
 							<h2 className={`${styles["mini-catalog__title"]} ${styles["mini-catalog__title--featured"]}`}>Featured Items</h2>
-							<div className="mini-catalog__btn-wrap">
-								<button className="mini-catalog__btn">
-									<svg>
-										<use></use>
+							<div className={styles["mini-catalog__button-wrap"]}>
+								<button className={styles["mini-catalog__button"]}>
+									<svg width="8" height="14">
+										<use href={arrowLeft}></use>
 									</svg>
 								</button>
-								<button className="mini-catalog__btn">
-									<svg>
-										<use></use>
+								<button className={styles["mini-catalog__button"]}>
+									<svg width="8" height="14">
+										<use href={arrowRight}></use>
 									</svg>
 								</button>
 							</div>
@@ -32,15 +35,15 @@ export const MiniCatalog = () => {
 					<div className={`${styles["mini-catalog__list-wrap"]} ${styles["mini-catalog__list-wrap--popular"]}`}>
 						<div className={`${styles["mini-catalog__top-wrap"]} ${styles["mini-catalog__top-wrap--popular"]}`}>
 							<h2 className={`${styles["mini-catalog__title"]} ${styles["mini-catalog__title--popular"]}`}>Most Popular</h2>
-							<div className="mini-catalog__btn-wrap">
-								<button className="mini-catalog__btn">
-									<svg>
-										<use></use>
+							<div className={styles["mini-catalog__button-wrap"]}>
+								<button className={styles["mini-catalog__button"]}>
+									<svg width="8" height="14">
+										<use href={arrowLeft}></use>
 									</svg>
 								</button>
-								<button className="mini-catalog__btn">
-									<svg>
-										<use></use>
+								<button className={styles["mini-catalog__button"]}>
+									<svg width="8" height="14">
+										<use href={arrowRight}></use>
 									</svg>
 								</button>
 							</div>
@@ -51,7 +54,7 @@ export const MiniCatalog = () => {
 							))}
 						</ul>
 					</div>
-				</div>
+				</Container>
 			</section>
 		</>
 	);
