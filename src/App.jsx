646387950e43c831aee9e8e8e27b5e3fbaf26ce1
Manko Yana <mankoalex17032007@gmail.com>
2025-07-26 16:17:57 +0300

    7

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import { HomePage } from "./pages/HomePage/HomePage.jsx";
+<<<<<<< Updated upstream
+=======
+import BrandList from "./components/BrandList/BrandList";
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <>
       <HomePage />
+            <BrandList /> 
+
     </>
   );
 }
