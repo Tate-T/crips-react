cc211150fe13fd06e8dc95c74d72eb0860c79b93
alex4682 <smoliarenko.sasha@gmail.com>
2025-07-26 13:50:29 +0300

    made sorting styles

--- src/pages/CatalogPage/Sorting/Sorting.jsx
+++ src/pages/CatalogPage/Sorting/Sorting.jsx
@@ -1,13 +1,14 @@
+import "./Sorting.scss";
 export const Sorting = () => {
   return (
-    <form action="">
-      <select name="sortBy" id="">
+    <form action="" className="sorting__form">
+      <select name="sortBy" id="" className="sorting__select-sortBy">
         <option value="">price (High to low)</option>
         <option value="">price (Low to high)</option>
         <option value="">popularity(High to low)</option>
         <option value="">popularity(Low to high)</option>
       </select>
-      <select name="sortNum" id="">
+      <select name="sortNum" id="" className="sorting__select-sortNum">
         <option value="">48</option>
         <option value="">49</option>
         <option value="">50</option>
