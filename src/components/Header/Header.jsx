23888ba808dcd4bf17513c9b2b82769adbc74a8b
Miha77777ua <lv11miha@gmail.com>
2025-07-26 14:06:27 +0300

    main change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,5 @@
+import { Container } from "../Container/Container";
+
 import Logo from "../../images/logo.svg";
 import Search from "../../images/search.svg";
 import Bag from "../../images/bag.svg";
@@ -8,7 +10,7 @@ import style from "./Header.module.scss";
 export const Header = () => {
   return (
     <header className={style.header}>
-      <div className="container">
+      <Container>
         <div className={style.header__wrap}>
           <img className={style.header__logo} src={Logo} alt="Logo SVG" />
           <div className={style.header__center}>
@@ -22,28 +24,32 @@ export const Header = () => {
               </ul>
             </nav>
             <div className={style.header__search}>
-              <img className={style.search__icon} src={Search} alt="Search SVG" />
-              <input className={style.search__input} type="text" placeholder="SEARCH" />
+              <label htmlFor="search">
+                <img className={style.search__icon} src={Search} alt="Search SVG" />
+              </label>
+              <input className={style.search__input} type="text" placeholder="SEARCH" id="search" />
             </div>
           </div>
           <div className={style.header__right}>
             <div className={style.header__authentication}>
-              <a className={style.header__sign_in} href=".">SIGN IN</a>
-              <a className={style.header__create} href=".">CREATE AN ACCOUNT</a>
+              <a className={style.auth__link} href=".">SIGN IN</a>
+              <a className={style.auth__link} href=".">CREATE AN ACCOUNT</a>
             </div>
-            <button className={style.header__like}>
-              <img src={Heart} alt="Heart SVG" className={style.header__like_svg} />
-            </button>
-            <a href=".">
-              <img src={Bag} alt="Bag SVG" />
-              <div>
-                <p>Shopping Cart</p>
-                <p>0,00 EUR</p>
-              </div>
-            </a>
+            <div className={style.header__actions}>
+              <button className={style.header__like}>
+                <img src={Heart} alt="Heart SVG" className={style.header__like_svg} />
+              </button>
+              <a href=".">
+                <img src={Bag} alt="Bag SVG" />
+                <div>
+                  <p>Shopping Cart</p>
+                  <p>0,00 EUR</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      </Container>
     </header>
   )
 }
