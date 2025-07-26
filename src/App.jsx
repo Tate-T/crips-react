9931dbaf4c53c1a8c025a95327814bcf555306a3
Yul1aPedchenko <136635600+Yul1aPedchenko@users.noreply.github.com>
2025-07-26 14:18:43 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import "./App.css";
 import { HomePage } from "./pages/HomePage/HomePage.jsx";
-import { CreateOrder } from "./pages/CabinetPage/CreateOrder/CreateOrder.jsx";
 function App() {
   return (
     <>
-      <CreateOrder></CreateOrder>
       <HomePage />
     </>
   );
