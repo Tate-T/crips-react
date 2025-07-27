835ebf1585d1f3fa4993fc4280486644a184b645
rocketmike12 <mg.korop@gmail.com>
2025-07-27 13:44:39 +0300

    adaptive mostly done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,20 +3,19 @@ import { Header } from "../../components/Header/Header.jsx";
 import { CreateOrder } from "../CabinetPage/CreateOrder/CreateOrder.jsx";
 import { CatalogSection } from "../CatalogPage/CatalogPage.jsx";
 import { MiniCatalog } from "../../components/MiniCatalog/MiniCatalog.jsx";
-import { Blog } from "../main-page/blog/blog.jsx"
-
+import { Blog } from "../main-page/blog/blog.jsx";
 
 export const HomePage = () => {
-  return (
-    <>
-      <Header />
-      <main>
-        {/* <MiniCatalog />
-        <CatalogSection /> */}
-        <CreateOrder />
-        <Blog />
-      </main>
-      {/* <Footer /> */}
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<main>
+				<MiniCatalog />
+				<CatalogSection />
+				<CreateOrder />
+				<Blog />
+			</main>
+			{/* <Footer /> */}
+		</>
+	);
 };
