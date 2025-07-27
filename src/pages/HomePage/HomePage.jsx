9408165fcef8f2f7248af51bfca0dd6774fd8594
EgorTkachuK <134726720+EgorTkachuK@users.noreply.github.com>
2025-07-27 12:28:05 +0200

    partialStylesFooterBanners

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import { Header } from "../../components/Header/Header.jsx";
 import { Footer } from "../../components/Footer/Footer.jsx";
 import { CreateOrder } from "../CabinetPage/CreateOrder/CreateOrder.jsx";
 import { CatalogSection } from "../CatalogPage/CatalogPage.jsx";
+import { Banners } from "../../components/Banners/BannersSection.jsx";
 import { MiniCatalog } from "../../components/MiniCatalog/MiniCatalog.jsx";
 import { Blog } from "../main-page/blog/blog.jsx"
 
@@ -10,6 +11,7 @@ export const HomePage = () => {
     <>
       <Header />
       <main>
+        <Banners/>
         <MiniCatalog />
         <CatalogSection />
         <CreateOrder />
