9c3397abd44d51226d04c904739b2254c78fda30
elen-purple <anna.havrylova.2011@gmail.com>
2025-07-27 13:19:23 +0300

    makes address adaptive

--- src/pages/Cabinet/DashboardPage/DashboardPage.jsx
+++ src/pages/Cabinet/DashboardPage/DashboardPage.jsx
@@ -1,5 +1,6 @@
 import { Header } from "../../../components/Header/Header.jsx";
 import { Dashboard } from "./Dashboard/Dashboard.jsx";
+import { AddNewAddress } from "./AddNewAddress/AddNewAddress.jsx";
 import { Footer } from "../../../components/Footer/Footer.jsx";
 
 export const DashboardPage = () => {
@@ -8,6 +9,7 @@ export const DashboardPage = () => {
       {/* <Header /> */}
       <main>
         <Dashboard />
+        <AddNewAddress />
       </main>
       {/* <Footer /> */}
     </>
