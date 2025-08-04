4bbfdb99bf01ee0b8df4c2347991a9b135a6a98c
Sokolove-Olexii <130235909+Sokolove-Olexii@users.noreply.github.com>
2025-08-04 07:57:22 -0400

    made html base and styles
    added some images, added <Hero/> tag and import to HomePage, needs some images for slider and description to them

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,14 +5,16 @@ import { CatalogSection } from "../CatalogPage/CatalogPage.jsx";
 import { Banners } from "../../components/Banners/BannersSection.jsx";
 import { MiniCatalog } from "../../components/MiniCatalog/MiniCatalog.jsx";
 import SpecialList from "../../components/SpecialList/SpecialList.jsx";
-import { Blog } from "../main-page/blog/blog.jsx"
-import { CardList } from "../../components/card-list/CardList.jsx"
+import { Blog } from "../main-page/blog/blog.jsx";
+import { CardList } from "../../components/card-list/CardList.jsx";
+import { Hero } from "../../components/Hero/Hero.jsx";
 
 export const HomePage = () => {
   return (
     <>
       <Header activePage="home" />
       <main>
+        <Hero />
         <Banners />
         {/* <MiniCatalog /> */}
         <Blog />
