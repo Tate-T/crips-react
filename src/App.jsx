49ddc86c0f60192d359d4bb8e03242d51e57e878
Брацлавський Захар <100858241+Bratslavskij@users.noreply.github.com>
2025-08-23 13:22:07 +0300

    more adaptive

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { HomePage } from "./pages/HomePage/HomePage.jsx";
-// import { ProductPage } from "./pages/ProductPage/ProductPage.jsx";
 import { ProductPage } from "./pages/ProductPage/ProductPage.jsx";
 import { CartPage } from "./pages/CartPage/CartPage.jsx";
 
 function App() {
   return (
     <>
+      <HomePage />
       <CartPage />
-      {/* <ProductPage/> */}
+      <ProductPage />
       {/* <CreateNewAcount></CreateNewAcount> */}
     </>
   );
--- src/pages/CatalogPage/Banner/Banner.jsx
+++ src/pages/CatalogPage/Banner/Banner.jsx
@@ -1,17 +1,22 @@
 import "./Banner.scss";
 import { Container } from '../../../components/Container/Container'
+import { Component } from "react";
 
-const Banner = () => {
-  return (
+export default class Banner extends Component {
+  render = () => (
     <div className="banner">
       <Container>
         <div className="banner__container">
           <div className="banner__content">
             <h1 className="banner__title">shoping without limits.</h1>
-            <p className="banner__subtitle">
+            <p className="banner__subtitle-d">
               You can choose the best option for you, and it does not matter
               whether you are in Prague or San Francisco. We will deliver your
               purchase anywhere!
+            </p>
+            <p className="banner__subtitile">
+              Choose the best option for you, and it does not matter whether you
+              are.
             </p>
             <button className="banner__btn">Shop Now</button>
           </div>
@@ -20,5 +25,3 @@ const Banner = () => {
     </div>
   );
 };
-
-export default Banner;
