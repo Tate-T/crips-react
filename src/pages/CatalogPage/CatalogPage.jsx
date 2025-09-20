7a10b7f4ab1884f589302ea36d63c11927465f3d
alex4682 <smoliarenko.sasha@gmail.com>
2025-09-20 14:23:13 +0300

    made catalog fixes

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -2,15 +2,22 @@ import { Filtration } from "./Filtration/Filtration.jsx";
 import { Header } from "../../components/Header/Header";
 import { FullCatalog } from "./Catalog/FullCatalog.jsx";
 import { Footer } from "../../components/Footer/Footer.jsx";
+import "./Catalog.scss";
+import { Container } from "../../components/Container/Container.jsx";
 
 export const CatalogPage = () => {
   return (
     <>
       <Header />
-      <main className="container">
-        <Filtration />
-        <FullCatalog />
-      </main>
+      <section className="catalogSection">
+        <Container>
+          <div>
+            <p className="catalog-way">Home / Womens Dress / Best Chose</p>
+            <Filtration />
+          </div>
+          <FullCatalog />
+        </Container>
+      </section>
       <Footer />
     </>
   );
