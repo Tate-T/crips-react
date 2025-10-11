a2735e7c130558cf4438aa8413b3f91c9b751e70
rocketmike12 <mg.korop@gmail.com>
2025-10-11 13:10:37 +0300

    class components -> hooks

--- src/components/MiniCatalog/MiniCatalog.jsx
+++ src/components/MiniCatalog/MiniCatalog.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 
 import { Container } from "../Container/Container.jsx";
 import { MiniCatalogItem } from "./MiniCatalogItem.jsx";
@@ -7,71 +7,67 @@ import { miniCatalogData } from "../../data/mini-catalog-data.js";
 
 import styles from "./MiniCatalog.module.scss";
 
-export class MiniCatalog extends Component {
-	constructor() {
-		super();
+export const MiniCatalog = function () {
+	const [data, setData] = useState(miniCatalogData);
+	const [currentPage, setCurrentPage] = useState(1);
+	const step = 8;
 
-		this.state = { data: miniCatalogData, currentPage: 1, step: 8 };
-	}
-
-	render() {
-		return (
-			<>
-				<section className={styles["mini-catalog"]}>
-					<Container>
-						<div className={styles["mini-catalog__wrap"]}>
-							<div className={styles["mini-catalog__main-wrap"]}>
-								<h2 className={styles["mini-catalog__main-title"]}>Shop Some Wear:</h2>
-								<ul className={styles["mini-catalog__main-list"]}>
-									<li className={styles["mini-catalog__main-list__item"]}>
-										<input className={styles["mini-catalog__main-list__checkbox"]} type="checkbox" />
-										<p className={styles["mini-catalog__main-list__text"]}>best sellers</p>
-									</li>
-									<li className={styles["mini-catalog__main-list__item"]}>
-										<input className={styles["mini-catalog__main-list__checkbox"]} type="checkbox" />
-										<p className={styles["mini-catalog__main-list__text"]}>new arrivals</p>
-									</li>
-									<li className={styles["mini-catalog__main-list__item"]}>
-										<input className={styles["mini-catalog__main-list__checkbox"]} type="checkbox" defaultChecked />
-										<p className={styles["mini-catalog__main-list__text"]}>top women</p>
-									</li>
-									<li className={styles["mini-catalog__main-list__item"]}>
-										<input className={styles["mini-catalog__main-list__checkbox"]} type="checkbox" />
-										<p className={styles["mini-catalog__main-list__text"]}>collection: summer</p>
-									</li>
-									<li className={styles["mini-catalog__main-list__item"]}>
-										<input className={styles["mini-catalog__main-list__checkbox"]} type="checkbox" />
-										<p className={styles["mini-catalog__main-list__text"]}>collection: spring</p>
-									</li>
-									<li className={styles["mini-catalog__main-list__item"]}>
-										<input className={styles["mini-catalog__main-list__checkbox"]} type="checkbox" />
-										<p className={styles["mini-catalog__main-list__text"]}>trending</p>
-									</li>
-								</ul>
-							</div>
-							<aside className={styles["mini-catalog__aside"]}>
-								<div className={styles["mini-catalog__list-wrap"]}>
-									<ul className={styles["mini-catalog__list"]}>
-										{this.state.data.slice(0, this.state.currentPage * this.state.step).map((item, id) => (
-											<MiniCatalogItem img={item.img} category={item.category} title={item.title} price={item.price} discountPrice={item.discountPrice} key={id} />
-										))}
-									</ul>
-									{this.state.data.slice(this.state.currentPage * this.state.step, (this.state.currentPage + 1) * this.state.step).length >= 4 && (
-										<button
-											className={styles["mini-catalog__load-button"]}
-											onClick={() => {
-												this.setState({ currentPage: this.state.currentPage + 1 });
-											}}
-										>
-											load more
-										</button>
-									)}
-								</div>
-							</aside>
+	return (
+		<>
+			<section className={styles["mini-catalog"]}>
+				<Container>
+					<div className={styles["mini-catalog__wrap"]}>
+						<div className={styles["mini-catalog__main-wrap"]}>
+							<h2 className={styles["mini-catalog__main-title"]}>Shop Some Wear:</h2>
+							<ul className={styles["mini-catalog__main-list"]}>
+								<li className={styles["mini-catalog__main-list__item"]}>
+									<input className={styles["mini-catalog__main-list__checkbox"]} type="checkbox" />
+									<p className={styles["mini-catalog__main-list__text"]}>best sellers</p>
+								</li>
+								<li className={styles["mini-catalog__main-list__item"]}>
+									<input className={styles["mini-catalog__main-list__checkbox"]} type="checkbox" />
+									<p className={styles["mini-catalog__main-list__text"]}>new arrivals</p>
+								</li>
+								<li className={styles["mini-catalog__main-list__item"]}>
+									<input className={styles["mini-catalog__main-list__checkbox"]} type="checkbox" defaultChecked />
+									<p className={styles["mini-catalog__main-list__text"]}>top women</p>
+								</li>
+								<li className={styles["mini-catalog__main-list__item"]}>
+									<input className={styles["mini-catalog__main-list__checkbox"]} type="checkbox" />
+									<p className={styles["mini-catalog__main-list__text"]}>collection: summer</p>
+								</li>
+								<li className={styles["mini-catalog__main-list__item"]}>
+									<input className={styles["mini-catalog__main-list__checkbox"]} type="checkbox" />
+									<p className={styles["mini-catalog__main-list__text"]}>collection: spring</p>
+								</li>
+								<li className={styles["mini-catalog__main-list__item"]}>
+									<input className={styles["mini-catalog__main-list__checkbox"]} type="checkbox" />
+									<p className={styles["mini-catalog__main-list__text"]}>trending</p>
+								</li>
+							</ul>
 						</div>
-					</Container>
-				</section>
-			</>
-		);
-	}
-}
+						<aside className={styles["mini-catalog__aside"]}>
+							<div className={styles["mini-catalog__list-wrap"]}>
+								<ul className={styles["mini-catalog__list"]}>
+									{data.slice(0, currentPage * step).map((item, id) => (
+										<MiniCatalogItem img={item.img} category={item.category} title={item.title} price={item.price} discountPrice={item.discountPrice} key={id} />
+									))}
+								</ul>
+								{data.slice(currentPage * step, (currentPage + 1) * step).length >= 4 && (
+									<button
+										className={styles["mini-catalog__load-button"]}
+										onClick={() => {
+											setCurrentPage(currentPage + 1);
+										}}
+									>
+										load more
+									</button>
+								)}
+							</div>
+						</aside>
+					</div>
+				</Container>
+			</section>
+		</>
+	);
+};
--- src/data/catalog-data.js
+++ src/data/catalog-data.js
@@ -1,4 +1,3 @@
-
 // import catalog1 from "../images/catalog/catalogImage1.jpg";
 // import catalog2 from "../images/catalog/catalog-image-2.jpg";
 // import catalog3 from "../images/catalog/catalog-image-3.jpg";
@@ -110,7 +109,6 @@
 //   },
 // ];
 
-
 // import catalog1 from "../images/catalog/desktop/catalog-image-1.jpg";
 // import catalog2 from "../images/catalog/desktop/catalog-image-2.jpg";
 // import catalog3 from "../images/catalog/desktop/catalog-image-3.jpg";
@@ -142,101 +140,101 @@
 //     img: !userMedia.matches ? catalog1Mobile : catalog1,
 
 export const catalogData = [
-  {
-    img: "https://i.postimg.cc/YqS20htm/catalog-Image1.jpg",
-    top: "Top Woman",
-    name: "Angels malu zip jeans slim black used",
-    price: "139,00 EUR",
-    colors: ["#24426A", "#18574A", "#666689"],
-  },
-  {
-    img: "https://i.postimg.cc/bNnCQRYX/catalog-image-2.jpg",
-    //     img: !userMedia.matches ? catalog2Mobile : catalog2,
-    top: "Top Woman",
-    name: "Angels malu zip jeans slim black used",
-    price: "235,00 EUR",
-    colors: ["#88ABCB", "#FACBCE", "#A75E5D"],
-  },
-  {
-    img: "https://i.postimg.cc/TPtCR70b/catalog-image-3.jpg",
-    //     img: !userMedia.matches ? catalog3Mobile : catalog3,
-    top: "Top Woman",
-    name: "Angels malu zip jeans slim black used",
-    price: "90,00 EUR",
-    colors: ["#C2BEB6", "#24426A", "#CBA13E"],
-  },
-  {
-    img: "https://i.postimg.cc/HkCS37jr/catalog-image-4.jpg",
-    //     img: !userMedia.matches ? catalog4Mobile : catalog4,
-    top: "Top Woman",
-    name: "Angels malu zip jeans slim black used",
-    price: "139,00 EUR",
-    colors: ["#24426A", "#F3ECE2", "#666689"],
-  },
-  {
-    img: "https://i.postimg.cc/YCfdPqmb/catalog-image-5.jpg",
-    //     img: !userMedia.matches ? catalog5Mobile : catalog5,
-    top: "Top Woman",
-    name: "Angels malu zip jeans slim black used",
-    price: "139,00 EUR",
-    colors: ["#2B9FA7", "#971E34", "#666689"],
-  },
-  {
-    img: "https://i.postimg.cc/6QWMxJrw/catalog-image-6.jpg",
-    //     img: !userMedia.matches ? catalog6Mobile : catalog6,
-    top: "Top Woman",
-    name: "Angels malu zip jeans slim black used",
-    price: "139,00 EUR",
-    colors: ["#666689", "#292A2D", "#F3ECE2"],
-    discount: 30,
-  },
-  {
-    img: "https://i.postimg.cc/bNnCQRYX/catalog-image-2.jpg",
-    //     img: !userMedia.matches ? catalog7Mobile : catalog7,
-    top: "Top Woman",
-    name: "Angels malu zip jeans slim black used",
-    price: "139,00 EUR",
-    colors: ["#88ABCB", "#FACBCE", "#A75E5D"],
-  },
-  {
-    img: "https://i.postimg.cc/9076w5gK/catalog-image-8.jpg",
-    //     img: !userMedia.matches ? catalog8Mobile : catalog8,
-    top: "Top Woman",
-    name: "Angels malu zip jeans slim black used",
-    price: "139,00 EUR",
-    colors: ["#F3ECE2", "#292A2D", "#AAABA7"],
-  },
-  {
-    img: "https://i.postimg.cc/HkCS37jr/catalog-image-4.jpg",
-    //     img: !userMedia.matches ? catalog9Mobile : catalog9,
-    top: "Top Woman",
-    name: "Angels malu zip jeans slim black used",
-    price: "139,00 EUR",
-    colors: ["#24426A", "#18574A", "#666689"],
-  },
-  {
-    img: "https://i.postimg.cc/YqS20htm/catalog-Image1.jpg",
-    //     img: !userMedia.matches ? catalog10Mobile : catalog10,
-    top: "Top Woman",
-    name: "Angels malu zip jeans slim black used",
-    price: "139,00 EUR",
-    colors: ["#88ABCB", "#FACBCE", "#A75E5D"],
-    discount: 30,
-  },
-  {
-    img: "https://i.postimg.cc/YCfdPqmb/catalog-image-5.jpg",
-    //     img: !userMedia.matches ? catalog11Mobile : catalog11,
-    top: "Top Woman",
-    name: "Angels malu zip jeans slim black used",
-    price: "139,00 EUR",
-    colors: ["#C2BEB6", "#24426A", "#CBA13E"],
-  },
-  {
-    img: "https://i.postimg.cc/9076w5gK/catalog-image-8.jpg",
-    //     img: !userMedia.matches ? catalog12Mobile : catalog12,
-    top: "Top Woman",
-    name: "Angels malu zip jeans slim black used",
-    price: "139,00 EUR",
-    colors: ["#24426A", "#F3ECE2", "#666689"],
-  },
+	{
+		img: "https://i.postimg.cc/YqS20htm/catalog-Image1.jpg",
+		top: "Top Woman",
+		name: "Angels malu zip jeans slim black used",
+		price: "139,00 EUR",
+		colors: ["#24426A", "#18574A", "#666689"]
+	},
+	{
+		img: "https://i.postimg.cc/bNnCQRYX/catalog-image-2.jpg",
+		//     img: !userMedia.matches ? catalog2Mobile : catalog2,
+		top: "Top Woman",
+		name: "Angels malu zip jeans slim black used",
+		price: "235,00 EUR",
+		colors: ["#88ABCB", "#FACBCE", "#A75E5D"]
+	},
+	{
+		img: "https://i.postimg.cc/TPtCR70b/catalog-image-3.jpg",
+		//     img: !userMedia.matches ? catalog3Mobile : catalog3,
+		top: "Top Woman",
+		name: "Angels malu zip jeans slim black used",
+		price: "90,00 EUR",
+		colors: ["#C2BEB6", "#24426A", "#CBA13E"]
+	},
+	{
+		img: "https://i.postimg.cc/HkCS37jr/catalog-image-4.jpg",
+		//     img: !userMedia.matches ? catalog4Mobile : catalog4,
+		top: "Top Woman",
+		name: "Angels malu zip jeans slim black used",
+		price: "139,00 EUR",
+		colors: ["#24426A", "#F3ECE2", "#666689"]
+	},
+	{
+		img: "https://i.postimg.cc/YCfdPqmb/catalog-image-5.jpg",
+		//     img: !userMedia.matches ? catalog5Mobile : catalog5,
+		top: "Top Woman",
+		name: "Angels malu zip jeans slim black used",
+		price: "139,00 EUR",
+		colors: ["#2B9FA7", "#971E34", "#666689"]
+	},
+	{
+		img: "https://i.postimg.cc/6QWMxJrw/catalog-image-6.jpg",
+		//     img: !userMedia.matches ? catalog6Mobile : catalog6,
+		top: "Top Woman",
+		name: "Angels malu zip jeans slim black used",
+		price: "139,00 EUR",
+		colors: ["#666689", "#292A2D", "#F3ECE2"],
+		discount: 30
+	},
+	{
+		img: "https://i.postimg.cc/bNnCQRYX/catalog-image-2.jpg",
+		//     img: !userMedia.matches ? catalog7Mobile : catalog7,
+		top: "Top Woman",
+		name: "Angels malu zip jeans slim black used",
+		price: "139,00 EUR",
+		colors: ["#88ABCB", "#FACBCE", "#A75E5D"]
+	},
+	{
+		img: "https://i.postimg.cc/9076w5gK/catalog-image-8.jpg",
+		//     img: !userMedia.matches ? catalog8Mobile : catalog8,
+		top: "Top Woman",
+		name: "Angels malu zip jeans slim black used",
+		price: "139,00 EUR",
+		colors: ["#F3ECE2", "#292A2D", "#AAABA7"]
+	},
+	{
+		img: "https://i.postimg.cc/HkCS37jr/catalog-image-4.jpg",
+		//     img: !userMedia.matches ? catalog9Mobile : catalog9,
+		top: "Top Woman",
+		name: "Angels malu zip jeans slim black used",
+		price: "139,00 EUR",
+		colors: ["#24426A", "#18574A", "#666689"]
+	},
+	{
+		img: "https://i.postimg.cc/YqS20htm/catalog-Image1.jpg",
+		//     img: !userMedia.matches ? catalog10Mobile : catalog10,
+		top: "Top Woman",
+		name: "Angels malu zip jeans slim black used",
+		price: "139,00 EUR",
+		colors: ["#88ABCB", "#FACBCE", "#A75E5D"],
+		discount: 30
+	},
+	{
+		img: "https://i.postimg.cc/YCfdPqmb/catalog-image-5.jpg",
+		//     img: !userMedia.matches ? catalog11Mobile : catalog11,
+		top: "Top Woman",
+		name: "Angels malu zip jeans slim black used",
+		price: "139,00 EUR",
+		colors: ["#C2BEB6", "#24426A", "#CBA13E"]
+	},
+	{
+		img: "https://i.postimg.cc/9076w5gK/catalog-image-8.jpg",
+		//     img: !userMedia.matches ? catalog12Mobile : catalog12,
+		top: "Top Woman",
+		name: "Angels malu zip jeans slim black used",
+		price: "139,00 EUR",
+		colors: ["#24426A", "#F3ECE2", "#666689"]
+	}
 ];
