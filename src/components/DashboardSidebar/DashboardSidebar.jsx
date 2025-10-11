8c4d05f4180b544a069aa2e32e1a5128f0272b41
lika-ruby <mary.havrylova.2011@gmail.com>
2025-10-11 13:42:47 +0300

    makes all

--- src/components/DashboardSidebar/DashboardSidebar.jsx
+++ src/components/DashboardSidebar/DashboardSidebar.jsx
@@ -6,33 +6,117 @@ export class DashboardSidebar extends Component {
     return (
       <div className={styles.dashbar}>
         <ul onClick={this.props.changePage} className={styles.dashbar__list}>
-          <li id="dashSide1" className={styles.dashbar__item__active}>
-            <a className={styles.dashbar__link__active} href="#">
+          <li
+            id="dashSide1"
+            className={
+              this.props.page === "dashSide1"
+                ? styles.dashbar__item__active
+                : styles.dashbar__item
+            }
+          >
+            <a
+              className={
+                this.props.page === "dashSide1"
+                  ? styles.dashbar__item__active
+                  : styles.dashbar__item
+              }
+              href="#"
+            >
               Account Dashboard
             </a>
           </li>
-          <li id="dashSide2" className={styles.dashbar__item}>
-            <a className={styles.dashbar__link} href="#">
+          <li
+            id="dashSide2"
+            className={
+              this.props.page === "dashSide1"
+                ? styles.dashbar__item__active
+                : styles.dashbar__item
+            }
+          >
+            <a
+              className={
+                this.props.page === "dashSide1"
+                  ? styles.dashbar__link__active
+                  : styles.dashbar__link
+              }
+              href="#"
+            >
               Account Information
             </a>
           </li>
-          <li id="dashSide3" className={styles.dashbar__item}>
-            <a className={styles.dashbar__link} href="#">
+          <li
+            id="dashSide3"
+            className={
+              this.props.page === "dashSide1"
+                ? styles.dashbar__item__active
+                : styles.dashbar__item
+            }
+          >
+            <a
+              className={
+                this.props.page === "dashSide1"
+                  ? styles.dashbar__item__active
+                  : styles.dashbar__item
+              }
+              href="#"
+            >
               Address Book
             </a>
           </li>
-          <li id="dashSide4" className={styles.dashbar__item}>
-            <a className={styles.dashbar__link} href="#">
+          <li
+            id="dashSide4"
+            className={
+              this.props.page === "dashSide1"
+                ? styles.dashbar__item__active
+                : styles.dashbar__item
+            }
+          >
+            <a
+              className={
+                this.props.page === "dashSide1"
+                  ? styles.dashbar__item__active
+                  : styles.dashbar__item
+              }
+              href="#"
+            >
               My Orders
             </a>
           </li>
-          <li id="dashSide5" className={styles.dashbar__item}>
-            <a className={styles.dashbar__link} href="#">
+          <li
+            id="dashSide5"
+            className={
+              this.props.page === "dashSide1"
+                ? styles.dashbar__item__active
+                : styles.dashbar__item
+            }
+          >
+            <a
+              className={
+                this.props.page === "dashSide1"
+                  ? styles.dashbar__item__active
+                  : styles.dashbar__item
+              }
+              href="#"
+            >
               My Wishlist
             </a>
           </li>
-          <li id="dashSide6" className={styles.dashbar__item}>
-            <a className={styles.dashbar__link} href="#">
+          <li
+            id="dashSide6"
+            className={
+              this.props.page === "dashSide1"
+                ? styles.dashbar__item__active
+                : styles.dashbar__item
+            }
+          >
+            <a
+              className={
+                this.props.page === "dashSide1"
+                  ? styles.dashbar__item__active
+                  : styles.dashbar__item
+              }
+              href="#"
+            >
               Newsletter Subscriptions
             </a>
           </li>
