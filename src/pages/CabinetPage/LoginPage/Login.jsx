1e42e649bc342b40d06ceb8a301c5eafd1e912ee
rocketmike12 <mg.korop@gmail.com>
2025-10-12 13:29:47 +0300

    minor CreateNewAccount fixes; LoginPage created

--- src/pages/CabinetPage/LoginPage/Login.jsx
+++ src/pages/CabinetPage/LoginPage/Login.jsx
@@ -1,9 +1,9 @@
-import s from "./CreateNewAcount.module.scss";
-import { Container } from "../../../../components/Container/Container";
+import s from "./Login.module.scss";
+import { Container } from "../../../components/Container/Container";
 // import { Component } from "react";
 import { useState } from "react";
 
-export function CreateNewAcount() {
+export function Login() {
 	const [firstName, setFirstName] = useState("");
 	const [lastName, setlastName] = useState("");
 	const [email, setEmail] = useState("");
@@ -23,29 +23,26 @@ export function CreateNewAcount() {
 	//   setState({ [name]: value });
 	// };
 
-	 const handleSubmit = (e) => {
+	const handleSubmit = (e) => {
 		e.preventDefault();
 
 		console.log(e);
 
 		const form = e.currentTarget;
 
-		setFirstName(form.elements.firstName.value);
-		setlastName(form.elements.lastName.value);
 		setEmail(form.elements.email.value);
 		setPassword(form.elements.password.value);
-		setconfirmPassword(form.elements.confirmPassword.value);
 
 		// this.setState({ firstName: "" });
 		// this.setState({ lastName: "" });
 		// this.setState({ email: "" });
 		// this.setState({ password: "" });
 		// this.setState({ confirmPassword: "" });
-		
-		console.log(firstName, lastName, email, password, confirmPassword)
+
+		console.log(firstName, lastName, email, password, confirmPassword);
 
 		form.reset();
-	}
+	};
 
 	// render() {
 	//   console.log(this.firstName)
@@ -56,47 +53,13 @@ export function CreateNewAcount() {
 
 	return (
 		<>
-			<section className={s.createAcountSecti}>
+			<section className={s["login-section"]}>
 				<Container>
-					<p className={s.createMainText}>Home / Create New Customer Account</p>
-					<h2 className={s.createMainTitle}>Create New Customer Account</h2>
+					<p className={s.createMainText}>Home / Login</p>
+					<h2 className={s.createMainTitle}>Login</h2>
 					<div className={s.createMainBox}>
 						<form className={s.form} onSubmit={handleSubmit}>
 							<h2 className={s.createPersonalText}>Personal Information</h2>
-							<div className={s.createWrap}>
-								<label className={s.create__label}>
-									First name <span className={s.create__required}>*</span>
-								</label>
-								<input
-									className={s.createInput}
-									required
-									type="text"
-									name="firstName"
-									// value={firstName}
-									// onChange={(e)=>setFirstName(e.target.value)}
-									placeholder="firstName"
-								/>
-							</div>
-							<div className={s.createWrap}>
-								<label className={s.create__label}>
-									Last Name <span className={s.create__required}>*</span>
-								</label>
-								<input
-									className={s.createInput}
-									name="lastName"
-									type="text"
-									placeholder="lastName"
-									required
-									// onChange={(e)=>setlastName(e.target.value)}
-									// value={lastName}
-								/>
-							</div>
-							<label className={s.create__labelSign}>
-								<input type="checkbox" name="payment" value="card" />
-								Sign Up for Newsletter
-							</label>
-
-							<h2 className={s.createPersonalTextSec}>Sign Up for Newsletter</h2>
 							<div className={s.createWrap}>
 								<label className={s.create__label}>
 									Email <span className={s.create__required}>*</span>
@@ -125,23 +88,9 @@ export function CreateNewAcount() {
 									// value={password}
 								/>
 							</div>
-							<div className={s.createWrap}>
-								<label className={s.create__label}>
-									Confirm Password <span className={s.create__required}>*</span>
-								</label>
-								<input
-									className={s.createInput}
-									type="password"
-									name="confirmPassword"
-									placeholder=""
-									required
-									// onChange={(e)=>setconfirmPassword(e.target.value)}
-									// value={confirmPassword}
-								/>
-							</div>
 							<div className={s.btnWrap}>
 								<button className={s.createBtn} type="submit">
-									create an account
+									login	
 								</button>
 								<button className={s.backBtn}>Back</button>
 							</div>
---
+++ src/pages/CabinetPage/LoginPage/LoginPage.jsx
@@ -0,0 +1,9 @@
+import { Login } from "./Login.jsx";
+
+export const LoginPage = function () {
+	return (
+		<>
+			<Login />
+		</>
+	);
+};
