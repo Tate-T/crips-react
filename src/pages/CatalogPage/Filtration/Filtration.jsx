5c1f4d74b23d3cfbf0e8fabc9d83f42ea67404d8
alex4682 <smoliarenko.sasha@gmail.com>
2025-11-01 13:29:18 +0200

    fix bugs

--- src/pages/CatalogPage/Filtration/Filtration.jsx
+++ src/pages/CatalogPage/Filtration/Filtration.jsx
@@ -214,6 +214,7 @@ export const Filtration = () => {
     const toSlider = toSliderRef.current;
     const toInput = toInputRef.current;
   }, [fromValue, toValue]);
+  
 
   const resetAll = () => {
     setFromValue(0);
