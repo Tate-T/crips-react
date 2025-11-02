f008f17da0662c8866f08d2e52b5248aa4e203e2
der1x <130235333+Mark4353@users.noreply.github.com>
2025-11-02 14:45:04 +0300

    Table page

---
+++ src/data/table-data.js
@@ -0,0 +1,16 @@
+export const tableData = [
+  {
+    name: "Shane Cooper",
+    phone: "(907) 555-0101",
+    address: "beth.fernandez@example.com",
+    company: "Nintendo",
+    number: "817"
+  },
+  {
+    name: "Nathan Henry",
+    phone: "(207) 555-0119",
+    address: "craig.kelley@example.com",
+    company: "Pizza Hut",
+    number: "801"
+  }
+];
