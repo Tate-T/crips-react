f9aeaa5b100e8418bd022d33fe0511aaa14fa9a3
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2026-02-08 13:26:32 +0200

    blog-redux

--- src/pages/HomePage/Blog/Blog.jsx
+++ src/pages/HomePage/Blog/Blog.jsx
@@ -1,45 +1,10 @@
-import { useState } from "react";
+import { useSelector } from "react-redux";
 import style from "./Blog.module.scss";
 
-const allPostsData = [
-	{
-		id: 1,
-		category: "Article",
-		title: "Exactly What To Wear To Every Type Of Wedding This Summer",
-		excerpt:
-			'A large part of the business here is building up the "experience". Double-page spreads in magazines. Ads on TV. Product placement and sponsorship in golf tournaments, tennis matches and sailing competitions. All of this builds up your perception of the brand - and additionally, it costs money.',
-		date: "21 January 2018 by guido",
-		author: "guido"
-	},
-	{
-		id: 2,
-		category: "TIPS",
-		title: "Exactly What To Wear To Every Type Of Wedding This Summer",
-		excerpt: "When selling 70% fewer shirts, however, there are fewer shirts on the market, making it more exclusive, and therefore worth the money, for those to whom it matters. ",
-		date: "21 January 2018 by guido",
-		author: "elena"
-	},
-	{
-		id: 3,
-		category: "TIPS",
-		title: "Exactly What To Wear To Every Type Of Wedding This Summer",
-		excerpt:
-			'A large part of the business here is building up the "experience". Double-page spreads in magazines. Ads on TV. Product placement and sponsorship in golf tournaments, tennis matches and sailing competitions. All of this builds up your perception of the brand - and additionally, it costs money.',
-		date: "21 January 2018 by guido",
-		author: "mark"
-	},
-	{
-		id: 4,
-		category: "Article",
-		title: "Exactly What To Wear To Every Type Of Wedding This Summer",
-		excerpt: "When selling 70% fewer shirts, however, there are fewer shirts on the market, making it more exclusive, and therefore worth the money, for those to whom it matters. ",
-		date: "21 January 2018 by guido",
-		author: "guido"
-	}
-];
+
 
 export const Blog = () => {
-	const [posts, setPosts] = useState(allPostsData);
+const posts = useSelector((state) => state.blog.posts);
 
 	return (
 		<section id="blog" className={style.blog}>
--- src/redux/blog/reducer.js
+++ src/redux/blog/reducer.js
@@ -1,3 +1,49 @@
-export const blogReducer = (state = [], action) => {
-  return state;
+const allPostsData = [
+  {
+    id: 1,
+    category: "Article",
+    title: "Exactly What To Wear To Every Type Of Wedding This Summer",
+    excerpt:
+      'A large part of the business here is building up the "experience". Double-page spreads in magazines. Ads on TV. Product placement and sponsorship in golf tournaments, tennis matches and sailing competitions. All of this builds up your perception of the brand - and additionally, it costs money.',
+    date: "21 January 2018 by guido",
+    author: "guido"
+  },
+  {
+    id: 2,
+    category: "TIPS",
+    title: "Exactly What To Wear To Every Type Of Wedding This Summer",
+    excerpt: "When selling 70% fewer shirts, however, there are fewer shirts on the market, making it more exclusive, and therefore worth the money, for those to whom it matters. ",
+    date: "21 January 2018 by guido",
+    author: "elena"
+  },
+  {
+    id: 3,
+    category: "TIPS",
+    title: "Exactly What To Wear To Every Type Of Wedding This Summer",
+    excerpt:
+      'A large part of the business here is building up the "experience". Double-page spreads in magazines. Ads on TV. Product placement and sponsorship in golf tournaments, tennis matches and sailing competitions. All of this builds up your perception of the brand - and additionally, it costs money.',
+    date: "21 January 2018 by guido",
+    author: "mark"
+  },
+  {
+    id: 4,
+    category: "Article",
+    title: "Exactly What To Wear To Every Type Of Wedding This Summer",
+    excerpt: "When selling 70% fewer shirts, however, there are fewer shirts on the market, making it more exclusive, and therefore worth the money, for those to whom it matters. ",
+    date: "21 January 2018 by guido",
+    author: "guido"
+  }
+];
+
+
+
+export const blogReducer = (state = { posts: allPostsData }, action) => {
+  switch (action.type) {
+    case "GET_POSTS":
+      return {
+        ...state
+      };
+    default:
+      return state;
+  }
 };
