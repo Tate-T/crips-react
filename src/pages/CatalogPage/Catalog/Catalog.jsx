382a5b27fb4309aa39e8ef2159d71f0a653d4316
KaterinaZhuvak <katerinazhuvak@icloud.com>
2026-02-15 09:09:27 +0000

    before merge

--- src/pages/CatalogPage/Catalog/Catalog.jsx
+++ src/pages/CatalogPage/Catalog/Catalog.jsx
@@ -1,48 +1,64 @@
 import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 
-const Catalog = ({ style, img, name, category, colors, price, media, id, discount }) => {
-	const discountPrice = discount ? price - price * (discount / 100) : null;
+const Catalog = ({
+  id,
+  img,
+  name,
+  category,
+  colors,
+  price,
+  discountPrice,
+  media,
+  style
+}) => {
+  return (
+    <NavLink to={id} className={style.catalog__item}>
+    <img className={style.catalog__img} src={img} alt={name} />
 
-	return (
-		<NavLink to={id} className={style.catalog__item}>
-			<img className={style.catalog__img} src={img} alt="#" />
-			<p className={style.catalog__top}>{category}</p>
-			<h3 className={style.catalog__title}>{name}</h3>
+    <p className={style.catalog__top}>{category}</p>
+    <h3 className={style.catalog__title}>{name}</h3>
 
-			{discountPrice ? (
-				<>
-					<p className={style["catalog__price--discount"]}>
-						{parseFloat(discountPrice).toFixed(2) + " EUR"} <span className={style["catalog__price--old"]}>{parseFloat(price).toFixed(2) + " EUR"}</span>
-					</p>
-				</>
-			) : (
-				<>
-					<p className={style.catalog__price}>{parseFloat(price).toFixed(2) + " EUR"}</p>
-				</>
-			)}
+    {discountPrice ? (
+        <p className={style["catalog__price--discount"]}>
+          {parseFloat(discountPrice || 0).toFixed(2)} EUR{" "}
+          <span className={style["catalog__price--old"]}>
+            {parseFloat(price || 0).toFixed(2)} EUR
+          </span>
+        </p>
+      ) : (
+        <p className={style.catalog__price}>
+          {parseFloat(price || 0).toFixed(2)} EUR
+        </p>
+      )}
 
-			<ul className={style.catalog__colors}>
-				{media
-					? colors.map((item, index) => (
-							<li className={style.colors__item} key={index}>
-								<button type="button" className={style.catalog__color}>
-									<div className={style.color} style={{ backgroundColor: item }}></div>
-								</button>
-							</li>
-						))
-					: undefined}
-			</ul>
-		</NavLink>
-	);
-}
+      <ul className={style.catalog__colors}>
+        {media &&
+          colors.map((item, index) => (
+            <li className={style.colors__item} key={index}>
+              <button type="button" className={style.catalog__color}>
+                <div
+                  className={style.color}
+                style={{ backgroundColor: item }}
+                />
+              </button>
+            </li>
+        ))}
+      </ul>
+    </NavLink>
+  );
+};
 
 Catalog.propTypes = {
-	img: PropTypes.string.isRequired,
-	name: PropTypes.string.isRequired,
-	category: PropTypes.string.isRequired,
-	colors: PropTypes.array.isRequired,
-	price: PropTypes.string.isRequired,
-	style: PropTypes.object.isRequired
+  id: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  colors: PropTypes.array.isRequired,
+  price: PropTypes.string.isRequired,
+  discountPrice: PropTypes.string,
+  media: PropTypes.bool,
+  style: PropTypes.object.isRequired
 };
+
 export default Catalog;
--- src/pages/CatalogPage/Catalog/FullCatalog.jsx
+++ src/pages/CatalogPage/Catalog/FullCatalog.jsx
@@ -1,50 +1,50 @@
 import { connect } from "react-redux";
 import Catalog from "./Catalog.jsx";
 import Sorting from "../Sorting/Sorting.jsx";
-import Banner  from "../Banner/Banner.jsx";
-
+import Banner from "../Banner/Banner.jsx";
 import styles from "./Catalog.module.scss";
 
 const FullCatalog = ({ products }) => {
-	const userMedia = window.matchMedia("(min-width: 768px)").matches;
+  const userMedia = window.matchMedia("(min-width: 768px)").matches;
 
-	if (!products || products.length === 0) {
-		return <p>Products not found</p>;
-	}
+  if (!products || products.length === 0) {
+    return <p>Products not found</p>;
+  }
 
-	return (
-		<div className={styles.catalog}>
-			<Sorting />
+  return (
+    <div className={styles.catalog}>
+      <Sorting />
 
-			<ul className={styles.catalog__list}>
-				{products.map((item) => (
-					<Catalog
-						key={item.id}
-						{...item}
-						style={styles}
-						media={userMedia}
-					/>
-				))}
-			</ul>
+      <ul className={styles.catalog__list}>
+       {products.map((item, index) => (
+  <Catalog
+    key={item.id || index}
+    {...item}
+    style={styles}
+    media={userMedia}
+  />
+))}
 
-			<Banner />
+      </ul>
 
-			<ul className={styles.catalog__list}>
-				{products.map((item) => (
-					<Catalog
-						key={item.id}
-						{...item}
-						style={styles}
-						media={userMedia}
-					/>
-				))}
-			</ul>
-		</div>
-	);
-}
+      <Banner />
+
+      <ul className={styles.catalog__list}>
+        {products.map((item) => (
+          <Catalog
+            key={item.id}
+            {...item}
+            style={styles}
+            media={userMedia}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
-	products: state.products.products,
+  products: state.products.products,
 });
 
 export default connect(mapStateToProps)(FullCatalog);
