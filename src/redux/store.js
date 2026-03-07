bf1075fbecb3acf2a163d982ffc1843992c4eade
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2026-03-07 13:13:42 +0200

    persister store

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,18 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
- reducer: rootReducer,
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
